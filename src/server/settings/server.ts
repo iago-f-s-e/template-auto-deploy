efe93658dd8ecdece7f152bc4ea1c86476fd77a8
iago-f-s-e <iago.fagundes@outlook.com>
2022-07-28 23:26:26 -0300

    fix: envs

--- src/server/settings/server.ts
+++ src/server/settings/server.ts
@@ -3,7 +3,3 @@ export const HOST = process.env.HOST || '0.0.0.0';
 export const PORT = process.env.PORT || 8080;
 
 export const IS_PRODUCTION = process.env.NODE_ENV === 'production';
-
-export const STORE_ID = Number(process.env.STORE_ID) || 0;
-
-export const STORE_GROUP_ID = Number(process.env.STORE_GROUP_ID) || 0;
